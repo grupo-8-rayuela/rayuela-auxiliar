ed7fc4655b5b84122f41237f8bea2a09673fc49b
RominaRodes <rominarodes@gmail.com>
2022-06-03 09:01:30 -0300

    Estructura completa con servidor y styles

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+
+app.listen(3031, () => console.log("servidor escuchando puerto 3031 funcionando"))
