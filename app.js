dddc2d1a005e33257e11b68ba9f932145f9456e8
RominaRodes <rominarodes@gmail.com>
2022-06-03 09:25:11 -0300

    paginas html creadas y linkeadas subidas al servidor

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.get('/', (req,res) => {
+
+
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+});
+
+app.get('/productCategory', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCategory.html'))
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
 
 
